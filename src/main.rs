648f716152fb25339ed58ea08cc67f0abd71a4c1
Gil Mizrahi <gil@hasura.io>
2023-06-28 12:12:55 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use ndc_postgres::*;
+
+use axum::{
+    body::{Bytes, Full},
+    response::Json,
+    response::Response,
+    routing::get,
+    routing::post,
+    Router,
+};
+
+use axum::extract::{Path, Query};
+use serde_json::Value;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/id/:id", get(id))
+        .route("/json", post(json))
+        .route("/select", get(select));
+
+    let server =
+        axum::Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service());
+
+    println!("Starting axum server at 0.0.0.0:3000");
+
+    server.await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "hi"
+}
+
+async fn id(
+    Path(user_id): Path<i64>,
+    Query(params): Query<HashMap<String, String>>,
+) -> Response<Full<Bytes>> {
+    Response::builder()
+        .header("x-powered-by", "benchmark")
+        .header("Content-Type", "text/plain")
+        .body(Full::from(format!("{} {}", user_id, params["name"])))
+        .unwrap()
+}
+
+async fn json(Json(payload): Json<serde_json::Value>) -> Json<Value> {
+    Json(payload)
+}
+
+async fn select() -> Json<Vec<output::RowSet>> {
+    let plan = translate(input::QueryRequest {
+        root_field: "bamba".to_string(),
+        query: input::Query::Raw("my query".to_string()),
+    });
+    let output::QueryResponse(results) = execute(plan);
+    Json(results)
+}
