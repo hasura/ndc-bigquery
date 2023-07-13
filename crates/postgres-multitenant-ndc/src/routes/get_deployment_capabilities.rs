b4e235163669093ca3df512853bf5ad4657f831b
Gil Mizrahi <gil@hasura.io>
2023-07-13 16:39:09 +0300

    Clarify capabilities (#48)
    * Specify capabilities as they are today
    
    * capabilities are the same across configuration

--- crates/postgres-multitenant-ndc/src/routes/get_deployment_capabilities.rs
+++ crates/postgres-multitenant-ndc/src/routes/get_deployment_capabilities.rs
@@ -1,29 +1,12 @@
 use axum::Json;
-use gdc_client::models::{
-    Capabilities, CapabilitiesResponse, MutationCapabilities, QueryCapabilities,
-};
 
+use super::get_capabilities::get_capabilities;
 use crate::extract::Configuration;
+use gdc_client::models::CapabilitiesResponse;
 
 #[axum_macros::debug_handler(state = crate::state::ServerState)]
 pub async fn get_deployment_capabilities(
     Configuration(_configuration): Configuration,
 ) -> Json<CapabilitiesResponse> {
-    let empty = serde_json::to_value(()).unwrap();
-    Json(CapabilitiesResponse {
-        versions: "^1.0.0".into(),
-        capabilities: Capabilities {
-            explain: Some(empty.clone()),
-            query: Some(QueryCapabilities {
-                foreach: Some(empty.clone()),
-                order_by_aggregate: Some(empty.clone()),
-                relation_comparisons: Some(empty.clone()),
-            }),
-            mutations: Some(MutationCapabilities {
-                returning: Some(empty.clone()),
-                nested_inserts: Some(empty.clone()),
-            }),
-            relationships: Some(empty),
-        },
-    })
+    get_capabilities().await
 }
