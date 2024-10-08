c4abb8fa3099e76c49bf049866129f660a203c9d
Karthikeyan C <karthikeyan@hasura.io>
2024-10-08 17:01:49 +0530

    fix the capabilities

--- crates/connectors/ndc-bigquery/src/capabilities.rs
+++ crates/connectors/ndc-bigquery/src/capabilities.rs
@@ -10,24 +10,24 @@ pub fn get_capabilities() -> models::Capabilities {
     models::Capabilities {
         query: models::QueryCapabilities {
             aggregates: Some(models::LeafCapability {}),
-            variables: Some(models::LeafCapability {}),
+            variables: None,
             explain: Some(models::LeafCapability {}),
             exists: models::ExistsCapabilities {
                 nested_collections: Some(models::LeafCapability {}),
             },
             nested_fields: models::NestedFieldCapabilities {
-                filter_by: Some(models::LeafCapability {}),
-                order_by: Some(models::LeafCapability {}),
+                filter_by: None,
+                order_by: None,
                 aggregates: None,
             },
         },
         mutation: models::MutationCapabilities {
-            transactional: Some(models::LeafCapability {}),
-            explain: Some(models::LeafCapability {}),
+            transactional: None,
+            explain: None,
         },
         relationships: Some(models::RelationshipCapabilities {
             relation_comparisons: Some(models::LeafCapability {}),
-            order_by_aggregate: Some(models::LeafCapability {}),
+            order_by_aggregate: None,
         }),
     }
 }
